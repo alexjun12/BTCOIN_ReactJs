632efb9f6a32d4c1112cb3733ca9a4cf56a4cc80
alexjun12 <alexjun12@naver.com>
2022-01-06 05:49:53 +0900

    2022_01_06commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import Button from "./Button";
+
+import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => {
+    setValue((prev) => prev + 1);
+  }
+  const onChange = (event) => {
+    setKeyword(event.target.value);
+  }
+
+  console.log("Always");
+  useEffect(() => {console.log("Call the API...")},[]);
+  useEffect(() => {
+    if(keyword !== "" && keyword.length > 5){
+      console.log("SEARCH FOR", keyword);
+    }
+  }, [keyword]);
+  
   return (
     <div>
-      <h1>Welcome back!!!</h1>
+      <input value = {keyword} onChange={onChange} type = "text" placeholder="Search here..." /> 
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
